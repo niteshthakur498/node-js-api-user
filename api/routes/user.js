a89b66344542e2d295e71c77b8637a5f98eb362c
Nitesh Thakur <niteshthakur498@gmail.com>
2019-03-14 12:43:54 +0530

    Post route and controller added without hashing

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,13 +3,7 @@ const router = express.Router();
 
 
 
-router.get('/',(req,res) => {
-    res.send('User')
-});
 
 
-router.post('/',(req,res) => {
-    res.send('User')
-});
 
 module.exports = router;
