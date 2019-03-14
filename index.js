ef91baafc4770325e2f2a0edbd7f207266068560
Nitesh Thakur <niteshthakur498@gmail.com>
2019-03-15 01:13:06 +0530

    setup for unavailable routes

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost:27017/user-api" ,
 
 
 mongoose.Promise = global.Promise;
+
+
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method ==='OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+
 app.use('/api/user',userRoutes);
 
 
@@ -28,6 +45,21 @@ app.get('/',(req,res) => {
 
 
 
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 
 app.listen(PORT,(err) => {
